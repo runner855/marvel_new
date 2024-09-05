d3e37d6c6f54e73a0510a81fcf1250b342f985ec
Dario Milani <dariomilani855@gmail.com>
2024-09-06 00:16:40 +0200

    retrieving characters data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import charactersCall from "./api/charactersCall";
 
 export const App = () => {
+  useEffect(() => {
+    charactersCall
+      .get(`characters?apikey=${process.env.REACT_APP_API_KEY}`, {})
+      .then((res) => console.log(res));
+  }, []);
+  console.log(process.env.REACT_APP_API_KEY);
   return <div className="App">Marvel</div>;
 };
